83b2239e0e3d5be4bf4c3c18b743031297c1b64d
LehaGer <leha.ger97@mail.ru>
2020-10-25 21:24:24 +0300

    feat: implemrnt morze-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    
+    let CurrLetterBin = "";
+    let CurrLetterMorz = "";
+    let Str = "";
+
+    for(let i = 0; i < expr.length; i+=10){
+        if(expr.substring(i,i+10) === "**********"){
+            Str += " ";
+        }else{
+            CurrLetterBin = expr.substring(i,i+10).substring(expr.substring(i,i+10).indexOf("1"));
+            for(let y = 0; y < CurrLetterBin.length; y += 2){
+                if(CurrLetterBin.charAt(y) + CurrLetterBin.charAt(y+1) === "10")
+                    CurrLetterMorz += ".";
+                if(CurrLetterBin.charAt(y) + CurrLetterBin.charAt(y+1) === "11")
+                    CurrLetterMorz += "-";
+            }
+
+            Str += MORSE_TABLE[CurrLetterMorz];
+        }
+        
+        CurrLetterBin = "";
+        CurrLetterMorz = "";
+    }
+
+    return Str;
+
 }
 
 module.exports = {
